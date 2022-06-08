4014f02357a25a76478590e5b7e34d240ebf5cd2
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-06-08 23:59:15 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses")
+
+
+
+
+
+
+
+
+
+
+// let sayi = Math.round(Math.random() * 20);
+// let live = 5;
+
+
+
+
+// if (live > 0) {
+//     while (live >= 0) {
+//         let user = prompt("enter a number")
+//         if (user == sayi) {
+//             console.log("tebrikler");
+//             console.log(live);
+//             break
+//         } else if (
+//             user > sayi
+//         ) {
+//             console.log("asagi ininiz");
+//         } else if (user < sayi) {
+//             console.log("yukari cikiniz");
+//             console.log(live);
+//         } else
+
+//             live--
+
+//     };
+
+// } else {
+//     console.log("oyun hakkiniz bitmistir.");
+// }
